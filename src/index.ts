adc3264bf349ccfc2f69d2d36cf07284e118749b
Leon Enneken <git@leonenneken.de>
2021-09-18 00:54:59 +0200

    Uri updated

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export class MessageLogger {
 
-  private uri: string = 'https://message-logger.dev.shortbyte.me';
+  private uri: string = 'https://message-logger.dev.shortbyte.me/api';
 
   private accessToken: string;
 
